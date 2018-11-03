66e783a06751a40a9dbb766671836ebbe283e6d5
Michael Kirsch <michael.kirsch@jambit.com>
2018-11-03 18:36:54 +0100

    make sensor 3 work

--- src/sensor.py
+++ src/sensor.py
@@ -1,8 +1,11 @@
 from datetime import datetime
-from time import sleep
 from pymemcache.client import base
+from time import sleep
+
+
 def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
+
 
 # GrovePi + Grove Ultrasonic Ranger
 
@@ -25,45 +28,44 @@ timestamp4 = 0
 # SIG,NC,VCC,GND
 
 def read_SonicSensor(ultrasonic_ranger):
+    while True:
+        try:
+            # Read distance value from Ultrasonic
+            lastValue = ultrasonicRead(ultrasonic_ranger)
+            if (lastValue < 180 and lastValue > 1):
+                print get_timestamp()
+                print lastValue
+                # sleep(0.25)
+                return lastValue
+            else:
+                print "out of range sensor port " + ultrasonic_ranger
 
-   while True:
-      try:
-           # Read distance value from Ultrasonic
-           lastValue = ultrasonicRead(ultrasonic_ranger)
-           if(lastValue < 200 and lastValue > 3):
-              print get_timestamp()
-              print lastValue
-              sleep(0.25)
-              return lastValue
-           else:
-              print "out of range"
+        except TypeError:
+            print "TypeError sensor port " + str(ultrasonic_ranger)
+            # return "TypeError"
+        except IOError:
+            print "IOError"
+            # return "IOError"
 
-      except TypeError:
-          print "Error"
-          return "Error"
-      except IOError:
-          print "Error"
-          return "Error"
 
 # Create a handler for our read (GET) people
 def read():
-   print "sensor.read() called"
-   while True:
-      sensor1 = read_SonicSensor(2)
-      client = base.Client(('localhost', 11211))
+    print "sensor.read() called"
+    while True:
+        # sensor1 = read_SonicSensor(2)
+        client = base.Client(('localhost', 11211))
 
-      client.set('sensor1', read_SonicSensor(4))
-      client.set('sensor1_timestamp', get_timestamp())
+        client.set('sensor1', read_SonicSensor(4))
+        client.set('sensor1_timestamp', get_timestamp())
 
-      client.set('sensor2', read_SonicSensor(2))
-      client.set('sensor2_timestamp', get_timestamp())
+        # client.set('sensor2', read_SonicSensor(2))
+        # client.set('sensor2_timestamp', get_timestamp())
 
-      client.set('sensor3', read_SonicSensor(3))
-      client.set('sensor3_timestamp', get_timestamp())
+        # client.set('sensor3', read_sonicsensor(3))
+        # client.set('sensor3_timestamp', get_timestamp())
+        #
+        # client.set('sensor4', read_sonicsensor(8))
+        # client.set('sensor4_timestamp', get_timestamp())
 
-      client.set('sensor4', read_SonicSensor(8))
-      client.set('sensor4_timestamp', get_timestamp())
-
-      print "sensor values set to memcached"
-      sleep(0.25)
-
+        print "sensor values set to memcached"
+        sleep(0.1)
